1689b4fa33a93264116d2ba29f1b0dbd429a13d4
tanyo <tanyo@lightningstep.com>
2024-12-10 10:31:30 +0200

    remove some unused htlm

--- wp-content/themes/arcade/single-faqs.php
+++ wp-content/themes/arcade/single-faqs.php
@@ -1,24 +1,16 @@
-<?php 
-get_header(); 
-//$bgImage = get_the_post_thumbnail_url(null ,'full') ?: 'path-to-default-image.jpg';
-?>
- <?php while( have_posts() ) : the_post(); ?>
- 
- <?php 
- $hero_image_url = get_field('hero_post_image'); 
-  if ($hero_image_url)  :?>
-  <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('<?php echo $hero_image_url; ?>');">
-  <?php  else :?>
-  <div class="site-cover site-cover-sm same-height overlay single-page" style="background-color: #214252;">
-  <?php endif; ?>
+<?php get_header(); ?>
+
+<?php while( have_posts() ) : the_post(); ?>
   
+ <div class="site-cover site-cover-sm same-height overlay single-page" style="background-color: #214252;">
   <div class="container">
-      <div class="row same-height justify-content-center">
+     
+  <div class="row same-height justify-content-center">
         <div class="col-md-6">
           <div class="post-entry text-center">
               <h1 class="mb-4"><?php the_title(); ?></h1>
               <div class="post-meta align-items-center text-center">
-              <!-- <figure class="author-figure mb-0 me-3 d-inline-block"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure> -->
+              
               <span class="d-inline-block mt-1">By <?php echo get_the_author_meta( 'display_name' ); ?></span>
               <span class="d-inline-block mt-1">By <a class="text-white" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"> - <?php the_author(); ?></a></span> 
               <span>&nbsp;-&nbsp; <?php echo get_the_date(); ?></span>
@@ -62,15 +54,9 @@ get_header();
 							?>
 
           </div>
-          <!-- <div class="pt-4" style="display:flex">
-            Categories: <?php #the_category(); ?> &nbsp;
-            <?php #the_tags(); ?>
-          </div> -->
-
-
         </div>
         <?php endwhile; ?>
-        
+      
         <div class="col-md-12 col-lg-4 sidebar">
           <div class="sidebar-box">
           <h3 class="heading">FAQ Categories</h3>
